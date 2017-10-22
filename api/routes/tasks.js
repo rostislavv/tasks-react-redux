b8d2a00b589f0a3e08df0605873c154508fcc799
rosv <vrostislav@adforgeinc.com>
2017-10-22 13:17:35 +0200

    updated server, created table and added pre-defined tasks for users

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 const loggedIn = require('../lib/auth');
+const { query } = require('../database');
 
 router.get('/tasks',
   loggedIn,
   (req, res) => {
-    res.json([
-     { owner: 'admin', performer: 'admin',
-       state: 'open', date: '22.10.2017',
-       description: 'Do stuff'},
-       { owner: 'admin', performer: 'user', state: 'done', date: '21.10.2017', description: 'Done stuff'},
-       { owner: 'admin', performer: 'user', state: 'done', date: '20.10.2017', description: 'New task'}
-    ]);
+    query('SELECT * FROM tasks WHERE owner = ? OR performer = ?', [req.user.username, req.user.username], false)
+    .then(result => {
+      res.json(result);
+    })
   }
 );
 

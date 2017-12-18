71c39ee5006bf30fa5a6867bb58a446fbee39940
rosv <volskyirostislav@gmail.com>
2017-12-18 17:45:23 +0100

    added python

--- api/routes/regression.js
+++ api/routes/regression.js
@@ -4,22 +4,22 @@ const loggedIn = require('../lib/auth');
 const { query, run } = require('../database');
 const moment = require('moment');
 
+const { exec } = require('child_process');
+
 router.use(loggedIn);
 
 router.get('/regression',
   (req, res) => {
-    res.json({data: [
-      {x: 0, y: 8},
-      {x: 1, y: 5},
-      {x: 2, y: 4},
-      {x: 3, y: 9},
-      {x: 4, y: 1},
-      {x: 5, y: 7},
-      {x: 6, y: 6},
-      {x: 7, y: 3},
-      {x: 8, y: 2},
-      {x: 9, y: 0}
-    ]});
+    exec('./python/test.py', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    res.json(JSON.parse(stdout));
+
+  });
   }
 );
 
